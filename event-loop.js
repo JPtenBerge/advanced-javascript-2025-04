5c75aad7b1843f9bd3010a73070850b914f493e1
JP <jp@jp.nl>
2025-04-08 15:36:43 +0200

    Event loop

---
+++ event-loop.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+
+console.log('start');
+
+fs.readFile('boek.txt', 'utf8', (err, content) => {
+	console.log('content:', content);
+});
+
+setTimeout(() => {
+    console.log('timeout klaar!');
+}, 1000);
+
+
+requestAnimationFrame()
+
+for (let i = 0; i < 50000; i++) {
+    console.log('.', i);
+}
+
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
+console.log('klaar');
